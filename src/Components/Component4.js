cb0cf4bf73b353c1d1b3b28dd7a347b363641495
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-02 19:02:51 +0600

    new

--- src/Components/Component4.js
+++ src/Components/Component4.js
@@ -1,17 +1,4 @@
-// import React,{useContext} from "react";
-// import { UserContext } from "./UserContext";
-// const Component4 = () => {
-//    const {user, text} = useContext(UserContext);
-//   return (
-//     <div>
-//         <h1>{text}</h1>
-//       <h2>{user.id}</h2>
-//       <h4>{user.name}</h4>
-//     </div>
-//   );
-// };
 
-// export default Component4;
 
 
 import React,{useContext} from 'react';
